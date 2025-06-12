e8011aa62a93151f4114220514d6bf9e5702c475
FelixFriede <felix.friede@students.uni-mannheim.de>
2025-06-12 08:31:23 +0200

    Unignored everything.

---
+++ _site/assets/js/main.js
@@ -0,0 +1,36 @@
+
+function toggleMenu() {
+document.getElementById("menu").classList.add("open");
+document.getElementById("menuButton").style.display = "none";
+}
+
+function openSidebar(id) {
+closeSidebars(); // close others
+document.getElementById(id).classList.add("open");
+document.getElementById("overlay").classList.add("show");
+document.getElementById("menu").classList.remove("open");
+}
+
+function closeSidebars() {
+document.querySelectorAll('.sidebar').forEach(el => el.classList.remove("open"));
+document.getElementById("overlay").classList.remove("show");
+document.getElementById("menuButton").style.display = "inline-block";
+}
+
+function goHome() {
+window.location.href = "/index.html";
+}
+
+function scrollToTop() {
+window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+// Show/hide scroll button after scrolling
+window.addEventListener("scroll", function () {
+const scrollTopButton = document.getElementById("scrollTopButton");
+if (window.scrollY > window.innerHeight * 2) {
+    scrollTopButton.style.display = "inline-block";
+} else {
+    scrollTopButton.style.display = "none";
+}
+})
